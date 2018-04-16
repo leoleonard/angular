ac79f744318e9d218f6137104ac2307e624867b2
Damian Juzyk <damian.juzyk@gmail.com>
2018-04-16 16:46:16 +0200

    add dependency injections

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -2,4 +2,7 @@ import { LikeComponent } from './like.components';
 
 let component = new LikeComponent(10, true);
 component.onClick();
-console.log(`likesCount: ${component.likesCount}, isSelected: ${component.isSelected} `);
+console.log(`
+likesCount: ${component.likesCount},
+isSelected: ${component.isSelected}
+`);
